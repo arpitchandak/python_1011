164a6a02dbce0f21370727f25ba98947ef38d6f2
Arpit Chandak <arpitchandak123@gmail.com>
2019-04-08 18:35:21 +0530

    list_append

---
+++ list1.py
@@ -0,0 +1,4 @@
+x = [1,2,3,4,5,6]
+y = int(input("enter no. to append = "))
+x.append(y)
+print(x)
--- python_os.py
+++ python_os.py
@@ -1,8 +1,5 @@
-fd = "myname.txt"
+fd = "xyz.txt"
 
 file = open(fd, 'w')
-file.write("Hello")
+file.write("\n hlo arpit")
 file.close()
-file = open(fd, 'r')
-text = file.read()
-print(text)
